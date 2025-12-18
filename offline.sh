4e1a39c93c4f24c65c2bd0f5265165a3c4a74c49
wassfila <wassim.filali@gmail.com>
2025-12-18 14:58:23 +0100

    add tweaks to offline script

--- offline.sh
+++ offline.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
 source install/setup.bash
-ros2 launch rovi_description offline_view.launch.py
+
+ros2 daemon start >/dev/null 2>&1 || true
+
+ROS_LOCALHOST_ONLY=1 ros2 launch rovi_bringup offline_view.launch.py
 
